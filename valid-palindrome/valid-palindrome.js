3793c1f1f8a91227410ebe0a26a47cb643beb8e2
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-06-11 23:19:46 +0600

    Time: 73 ms, Memory: 49 MB - LeetHub

--- valid-palindrome/valid-palindrome.js
+++ valid-palindrome/valid-palindrome.js
@@ -7,19 +7,44 @@ var isPalindrome = function(s) {
         return true;
     }
     
-    const pureS = s.toLowerCase().split('').filter((letter) => {
-        return /[a-z0-9]/.test(letter);
-    })
+//     const pureS = s.toLowerCase().split('').filter((letter) => {
+//         return /[a-z0-9]/.test(letter);
+//     })
     
-    if (pureS.length < 2) {
-        return true;
-    }
+//     if (pureS.length < 2) {
+//         return true;
+//     }
     
-    for (let i = 0; i < (pureS.length - 1) / 2; i++) {
-        if (pureS[i] !== pureS.slice(-(i + 1))[0]) {
-            return false;
+    // for (let i = 0; i < (pureS.length - 1) / 2; i++) {
+    //     if (pureS[i] !== pureS.slice(-(i + 1))[0]) {
+    //         return false;
+    //     }
+    // }
+    
+    let i = 0;
+    let j = s.length - 1;
+    
+    do {
+        const iIsAlphanum = /[a-z0-9]/i.test(s[i]);
+        const jIsAlphanum = /[a-z0-9]/i.test(s[j]);
+        
+        if (!iIsAlphanum) {
+            i += 1;
         }
-    }
+        
+        if (!jIsAlphanum) {
+            j -= 1;
+        }
+        
+        if (iIsAlphanum && jIsAlphanum) {
+            if ((s[i].toLowerCase() !== s[j].toLowerCase())) {
+                return false;
+            }
+            
+            i += 1;
+            j -= 1; 
+        }
+    } while (i < j)
     
     return true;
 };
