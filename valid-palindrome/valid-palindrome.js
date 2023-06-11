4a6c103dd24cfd7e8782471be8d2e6785e90c2dc
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-06-11 23:22:00 +0600

    Time: 77 ms, Memory: 48.9 MB - LeetHub

--- valid-palindrome/valid-palindrome.js
+++ valid-palindrome/valid-palindrome.js
@@ -7,20 +7,6 @@ var isPalindrome = function(s) {
         return true;
     }
     
-//     const pureS = s.toLowerCase().split('').filter((letter) => {
-//         return /[a-z0-9]/.test(letter);
-//     })
-    
-//     if (pureS.length < 2) {
-//         return true;
-//     }
-    
-    // for (let i = 0; i < (pureS.length - 1) / 2; i++) {
-    //     if (pureS[i] !== pureS.slice(-(i + 1))[0]) {
-    //         return false;
-    //     }
-    // }
-    
     let i = 0;
     let j = s.length - 1;
     
