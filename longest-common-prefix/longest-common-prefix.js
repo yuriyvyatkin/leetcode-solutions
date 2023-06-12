044453f290a7705ff44a9086a9fd61f6ded06b5d
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-06-13 00:51:44 +0600

    Time: 68 ms, Memory: 44.8 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -7,14 +7,18 @@ var longestCommonPrefix = function(strs) {
         return strs[0];
     }
     
+    if (strs.some((item) => item === '')) {
+        return '';
+    }
+    
     let result = [];
     
     for (let i = 0; i <= strs.length - 2; i++) {
         let shortS, longS = '';
         
-        if (strs[i].length === 0 || strs[i + 1].length === 0) {
-            return '';
-        }
+        // if (strs[i].length === 0 || strs[i + 1].length === 0) {
+        //     return '';
+        // }
         
         if (strs[i].length < strs[i + 1].length) {
             shortS = strs[i];
@@ -27,23 +31,10 @@ var longestCommonPrefix = function(strs) {
         shortS = shortS.split('');
         longS = longS.split('');
         
-        console.log(shortS)
-        console.log(longS)
-        
         const comparisonSubstringLength = result.length || shortS.length;
         
         for (let j = 0; j < comparisonSubstringLength; j++) {
-            // if (j < result.length) {
-            //     if (shortS[j] !== result[j]) {
-            //         result = result.slice(0, j);
-            //         break;
-            //     } else if (shortS[j] === longS[j]) {
-                    
-            //     }
-            // }
-            
             if (shortS[j] === longS[j] && j < result.length) {
-                console.log('test1');
                 if (shortS[j] === result[j]) {
                     continue;
                 } else {
@@ -51,12 +42,9 @@ var longestCommonPrefix = function(strs) {
                     break;
                 }
             } else if (shortS[j] === longS[j]) {
-                console.log('test2');
                 result.push(shortS[j]);
                 continue;
             } else {
-                console.log('test3');
-                console.log(j)
                 result = result.slice(0, j);
                 if (result.length === 0) {
                     return '';
