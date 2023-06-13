d4da3595be5fefaa703c06dfa2863e733bd64a3a
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-06-13 12:08:24 +0600

    Time: 87 ms, Memory: 42.5 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -6,44 +6,29 @@ var longestCommonPrefix = function(strs) {
     if (strs.length === 1) {
         return strs[0];
     }
-    
-    // if (strs.some((item) => item === '')) {
-    //     return '';
-    // }
-    
+
     let result = '';
     
     for (let i = 0; i <= strs.length - 2; i++) {
         if (strs[i] === '') {
             return '';
         };
+
+        const firstS = strs[i];
+        const secondS = strs[i + 1];
         
-        let shortS, longS = '';
-        
-        if (strs[i].length < strs[i + 1].length) {
-            shortS = strs[i];
-            longS = strs[i + 1];
-        } else {
-            shortS = strs[i + 1];
-            longS = strs[i];
-        }
-        
-        // shortS = shortS.split('');
-        // longS = longS.split('');
-        
-        const comparisonSubstringLength = result.length || shortS.length;
+        const comparisonSubstringLength = result.length || firstS.length;
         
         for (let j = 0; j < comparisonSubstringLength; j++) {
-            if (shortS[j] === longS[j] && j < result.length) {
-                if (shortS[j] === result[j]) {
+            if (firstS[j] === secondS[j] && j < result.length) {
+                if (firstS[j] === result[j]) {
                     continue;
                 } else {
                     result = result.slice(0, j);
                     break;
                 }
-            } else if (shortS[j] === longS[j]) {
-                result += shortS[j];
-                // result.push(shortS[j]);
+            } else if (firstS[j] === secondS[j]) {
+                result += firstS[j];
                 continue;
             } else {
                 result = result.slice(0, j);
