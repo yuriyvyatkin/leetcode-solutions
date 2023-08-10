41d8302f2bad17f137f36da969bd736e74f8cf94
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-10 21:31:17 +0600

    Time: 52 ms, Memory: 43.9 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        
-        for (let j = i; j < nums.length; j++) {
-            while (i !== j && nums[j] === num) {
-                nums.splice(j, 1);
-            }
+    let i = 1;
+    let lastNum = nums[0];
+    
+    for (let j = 1; j < nums.length; j++) {
+        if (lastNum < nums[j]) {
+            lastNum = nums[i] = nums[j];
+            i++;
         }
     }
+    
+    return i;
 };
