4580340cc9c8bfbdeac72b0122066556b0e4b9bb
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-11 08:42:37 +0600

    Time: 47 ms, Memory: 44.7 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if (arr.length < 3) {
+        return false;
+    }
+    
+    if (arr[0] > arr[1]) {
+        return false;
+    }
+    
+    let track = 0;
+    
+    for (let i = 2; i < arr.length; i++) {
+        if (arr[i] === arr[i - 1]) {
+            return false;
+        }
+        
+        if (arr[i] < arr[i - 1] && arr[i - 2] < arr[i - 1] && track == 0) {
+            track = 1;
+            continue;
+        }
+        
+        if (arr[i] > arr[i - 1] && track == 1) {
+            return false;
+        }
+    }
+
+    return track == 1;
+};
