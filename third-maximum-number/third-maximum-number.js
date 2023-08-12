82b4cc5244666407860a0d1432ee056d7544cd76
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-12 09:54:46 +0600

    Time: 59 ms, Memory: 48.1 MB - LeetHub

--- third-maximum-number/third-maximum-number.js
+++ third-maximum-number/third-maximum-number.js
@@ -3,42 +3,14 @@
  * @return {number}
  */
 var thirdMax = function(nums) {
-    const highestNums = [nums[0], null, null];
+    const maxNumbers = new Set();
     
-    for (let i = 1; i < nums.length; i++) {
-        if (highestNums.includes(nums[i])) {
-            continue;
-        }
-        
-        for (let j = 0; j < highestNums.length; j++) {
-            // const highestNum = highestNums.findIndex
-            if (highestNums[j] < nums[i]) {
-                console.log(j)
-                for (let k = 1; k >= j; k--) {
-                    highestNums[k + 1] = highestNums[k];
-                }
-                console.log(i)
-                console.log(highestNums)
-                highestNums[j] = nums[i];
-                break;
-            }
-
-            if (highestNums[j] === null) {
-                highestNums[j] = nums[i];
-            }
+    for (const num of nums) {
+        maxNumbers.add(num);
+        if (maxNumbers.size > 3) {
+            maxNumbers.delete(Math.min(...maxNumbers));
         }
     }
     
-    console.log('result')
-    console.log(highestNums.filter(Number.isInteger))
-    
-    if (highestNums[2] === highestNums[1]) {
-        highestNums[2] = null;
-    }
-    
-    if (highestNums[1] === highestNums[0]) {
-        highestNums[1] = null;
-    }
-
-    return highestNums[2] !== null ? highestNums[2] : Math.max(...highestNums.filter(Number.isInteger));
+    return maxNumbers.size === 3 ? Math.min(...maxNumbers) : Math.max(...maxNumbers);
 };
