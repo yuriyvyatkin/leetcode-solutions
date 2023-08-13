e4738b3212848488f271379fb7c5dba90c7fd8db
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-13 11:46:44 +0600

    Time: 56 ms, Memory: 42.4 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -3,17 +3,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
-        if (digits[i] === 9) {
-            digits[i] = 0;
-
-            if (i === 0) {
-                digits.unshift(1);
-                return digits;
-            }
-        } else {
-            digits[i] += 1;
+    for (let i = digits.length - 1; i >= 0; --i) {
+        if (digits[i] < 9) {
+            ++digits[i];
             return digits;
+        } else {
+            digits[i] = 0;
         }
     }
+    
+    digits.unshift(1);
+    return digits;
 };
