86102874ba60a4072dcd8167e31c3d93af318172
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-14 07:30:11 +0600

    Time: 52 ms, Memory: 41.7 MB - LeetHub

---
+++ spiral-matrix/spiral-matrix.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    if (matrix.length === 0) return [];
+    
+    let result = [];
+    let rowStart = 0, rowEnd = matrix.length - 1;
+    let colStart = 0, colEnd = matrix[0].length - 1;
+    
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        // Перемещение вправо
+        for (let col = colStart; col <= colEnd; col++) {
+            result.push(matrix[rowStart][col]);
+        }
+        rowStart++;
+
+        // Перемещение вниз
+        for (let row = rowStart; row <= rowEnd; row++) {
+            result.push(matrix[row][colEnd]);
+        }
+        colEnd--;
+
+        // Перемещение влево
+        if (rowStart <= rowEnd) {
+            for (let col = colEnd; col >= colStart; col--) {
+                result.push(matrix[rowEnd][col]);
+            }
+        }
+        rowEnd--;
+
+        // Перемещение вверх
+        if (colStart <= colEnd) {
+            for (let row = rowEnd; row >= rowStart; row--) {
+                result.push(matrix[row][colStart]);
+            }
+        }
+        colStart++;
+    }
+    
+    return result;
+};
