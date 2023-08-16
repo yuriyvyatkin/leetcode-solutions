f423d0f3904f55a49bd8eb05b358bd105380f3b2
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-16 21:31:26 +0600

    Time: 49 ms, Memory: 42 MB - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -6,12 +6,14 @@
 var strStr = function(haystack, needle) {
     for (let i = 0; i <= haystack.length - needle.length; i++) {
         let match = true;
+        
         for (let j = 0; j < needle.length; j++) {
             if (haystack[i + j] !== needle[j]) {
                 match = false;
                 break;
             }
         }
+        
         if (match) return i;
     }
     
