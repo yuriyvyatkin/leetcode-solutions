fee2e40b8f1ad6caa8e2846229a8e474702eea34
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-16 20:14:29 +0600

    Time: 42 ms, Memory: 42.3 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -7,7 +7,7 @@ var longestCommonPrefix = function(strs) {
     
     let prefix = strs[0];
     
-    for(let i = 1; i < strs.length; i++) {
+    for (let i = 1; i < strs.length; i++) {
         while(strs[i].indexOf(prefix) !== 0) {
             prefix = prefix.substring(0, prefix.length - 1);
             
