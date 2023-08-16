4c39aab5bbef30c62a63724f0a3d2561c68e9546
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-16 20:07:11 +0600

    Time: 50 ms, Memory: 42.6 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -3,16 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return "";
-
+    if (strs.length === 0) return '';
+    
     let prefix = strs[0];
-
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-          prefix = prefix.substring(0, prefix.length - 1);
-          if (prefix === "") return "";
+    
+    for(let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            
+            if (prefix === '') return '';
         }
     }
-
+    
     return prefix;
 };
