48260094b17b914769fd6325ab37c9a2317850fc
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-16 21:05:49 +0600

    Time: 52 ms, Memory: 41.6 MB - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle); 
+    if (needle.length === 0) return 0;
+    
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let match = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) return i;
+    }
+    
+    return -1;
 };
