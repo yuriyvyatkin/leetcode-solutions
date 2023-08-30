544ef3f79f4edeb364b4352c3d7ba46cd88141f3
Юрий Вяткин <yuriyvyatkin@yandex.ru>
2023-08-31 00:02:23 +0600

    Time: 59 ms, Memory: 46 MB - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -5,18 +5,18 @@
  */
 var minSubArrayLen = function(target, nums) {
     let minLength = Number.MAX_SAFE_INTEGER;
-    let left = 0;
     let currentSum = 0;
-
-    for (let right = 0; right < nums.length; ++right) {
-        currentSum += nums[right];
-
+    let slowPointer = 0;
+    
+    for (let fastPointer = 0; fastPointer < nums.length; ++fastPointer) {
+        currentSum += nums[fastPointer];
+        
         while (currentSum >= target) {
-            minLength = Math.min(minLength, right - left + 1);
-            currentSum -= nums[left];
-            left++;
+            minLength = Math.min(minLength, fastPointer - slowPointer + 1);
+            currentSum -= nums[slowPointer];
+            ++slowPointer;
         }
     }
-
+    
     return (minLength !== Number.MAX_SAFE_INTEGER) ? minLength : 0;
 };
